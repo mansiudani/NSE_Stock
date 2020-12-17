f9c9e9dd6c2184d1704cbea5c055c08debd49bf1
Mansi Udani <60266549+mansiudani@users.noreply.github.com>
2020-12-17 00:02:31 +0000

    Update backtest.py

--- finrl/trade/backtest.py
+++ finrl/trade/backtest.py
@@ -52,7 +52,7 @@ def BackTestPlot(account_value,
 
     with pyfolio.plotting.plotting_context(font_scale=1.3):
         pyfolio.create_full_tear_sheet(returns = DRL_strat,
-                                       benchmark_rets=dow_strat, set_context=False)
+                                       benchmark_rets=dow_strat, set_context=False, round_trips =True, slippage=0)
 
 
 def backtest_strat(df):
